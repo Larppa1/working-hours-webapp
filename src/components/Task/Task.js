9cd1370cc4e36829c192f238c92866940ec7bbc0
Larppa1 <lari.juva7@gmail.com>
2022-11-26 19:03:05 +0200

    -

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import './Task.css'
 
 export default function Task(props) {
-    const [isTaskActive, setIsTaskActive] = useState(false)
+    const [isTaskActive, setIsTaskActive] = useState(Boolean(localStorage.getItem(`task${props.id}active`)))
     const [duration, setDuration] = useState(Number(props.duration))
 
     useEffect(() => {
@@ -70,26 +70,32 @@ export default function Task(props) {
     useEffect(() => {
         let interval = null;
 
-        isTaskActive
-            ? setInterval(() => {
+        if(isTaskActive) {
+            interval = setInterval(() => {
                 setDuration((duration) => duration + 1000);
             }, 1000)
-            : clearInterval(interval)
+            localStorage.setItem(`task${props.id}active`, true)
+        }else {
+            localStorage.removeItem(`task${props.id}active`)
+            clearInterval(interval)
+        }
         return () => {
+            localStorage.removeItem(`task${props.id}active`)
             clearInterval(interval);
         }
-    }, [isTaskActive])
+    }, [isTaskActive, props.id])
 
-    window.onbeforeunload = () => {
-        isTaskActive
-            ? localStorage.setItem(`task${props.id}`, true)
-            : localStorage.setItem(`task${props.id}`, false)
-        //TODO: Make setItem value an object, give it a boolean to represent if task is active and a string to represent window closing time (new Date())
+    window.onload = () => {
+        if(isTaskActive) {
+            setDuration(duration + 20000)
+            localStorage.removeItem(`task${props.id}windowCloseTime`)
+        }
     }
 
-    //Patch updated task duration value to json-server
-    const patchDurationValue = () => {
-        setIsTaskActive(false)
+    
+
+    //Patch new task duration to json-server
+    window.onbeforeunload = () => {
         fetch(`http://127.0.0.1:3010/upcomingTasks/${props.id}`, {
             method: 'PATCH',
             body: JSON.stringify({
@@ -99,6 +105,10 @@ export default function Task(props) {
                 'Content-Type': 'application/json; charset=UTF-8',
             },
         })
+
+        if(isTaskActive) {
+            localStorage.setItem(`task${props.id}windowCloseTime`, new Date())
+        }
     }
 
     return(
@@ -113,7 +123,7 @@ export default function Task(props) {
                 <p id={'taskDescription' + props.id} contentEditable="true" suppressContentEditableWarning="true" spellCheck="false">{props.description}</p>
             </section>
             <section>
-                <h5 id='taskDurationCounter' className={isTaskActive ? 'counterActive' : 'counterNotActive'} onClick={() => {isTaskActive ? patchDurationValue() : setIsTaskActive(true)}}>{Math.floor((duration / (1000 * 60 * 60)) % 24)}h {Math.floor((duration / (1000 * 60)) % 60).toFixed(0)}min {((duration % 60000) / 1000).toFixed(0)}s</h5>
+                <h5 id='taskDurationCounter' className={isTaskActive ? 'counterActive' : 'counterNotActive'} onClick={() => {isTaskActive ? setIsTaskActive(null) : setIsTaskActive(true)}}>{Math.floor((duration / (1000 * 60 * 60)) % 24)}h {Math.floor((duration / (1000 * 60)) % 60).toFixed(0)}min {((duration % 60000) / 1000).toFixed(0)}s</h5>
                 <span>
                     <h6 id={'taskFinishDate' + props.id} contentEditable="true" suppressContentEditableWarning="true" spellCheck="false">{props.finishDate}</h6>
                     <h6>&nbsp;klo&nbsp;</h6>
