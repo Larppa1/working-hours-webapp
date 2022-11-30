4468303e4cfef1cf9a3a9b887e52ca5640ef3798
Larppa1 <lari.juva7@gmail.com>
2022-11-30 13:23:16 +0200

    -

--- src/pages/Tasks/Tasks.js
+++ src/pages/Tasks/Tasks.js
@@ -13,7 +13,6 @@ export default function Tasks() {
     useEffect(() => {
         (async () => {
             setUpcomingTaskList(await getTasks())
-            console.log(upcomingTaskList)
         })()
     }, [])
 
